cc1c91916d769e0fad47c33c859e27d2f50771cb
MuhammadFikriiii <tpublicml@gmail.com>
2025-06-17 03:04:39 +0800

    last

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,20 +1,30 @@
-import type { Metadata } from 'next'
-import './globals.css'
+import type { Metadata } from "next";
+import Script from "next/script"; // ✅ Tambahkan ini
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'CV generator',
-  description: 'Created by Muhammad Fikri',
-  generator: 'Muhammad Fikri',
-}
+  title: "CV generator",
+  description: "Created by Muhammad Fikri",
+  generator: "Muhammad Fikri",
+};
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Google AdSense script di sini */}
+        <Script
+          id="adsbygoogle-init"
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4642094843478665"
+          crossOrigin="anonymous"
+        />
+      </head>
       <body>{children}</body>
     </html>
-  )
+  );
 }
